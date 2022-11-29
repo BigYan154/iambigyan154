3dbc3a7522f73a65945e60a2cd3fe0a6425abd59
iambigyan154 <iambigyan.git@gmail.com>
2022-11-29 05:00:34 -0500

    Project Updated

--- first_project.py
+++ first_project.py
@@ -7,3 +7,11 @@ print("I live in "+ address)
 #  git config --global user.email "you@example.com"
 #   git config --global user.name "Your Name"
 
+# to add file to staging area:
+# git add filename.extension
+# git add filename.extension filename2.extension => two file
+# git add . => all file
+
+
+# to commit the change
+# git commit -m "Message"
