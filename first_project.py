0cc10d2a3249ed344af207eb1d7b95a2488c2812
iambigyan154 <iambigyan.git@gmail.com>
2022-11-29 04:54:16 -0500

    first project

--- first_project.py
+++ first_project.py
@@ -2,3 +2,8 @@ name = input("What is your name?")
 print("My name is "+ name)
 address = input("Where do you live?")
 print("I live in "+ address)
+
+# git config 
+#  git config --global user.email "you@example.com"
+#   git config --global user.name "Your Name"
+
